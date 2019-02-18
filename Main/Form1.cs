60d85cf5465d8918a1bb90969907cf92caf6c089
hunter95001 <hunter95001@naver.com>
2019-02-18 19:12:06 +0900

    2019-02-18 K-Means 소스 코드

---
+++ Main/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using K_Means.Main;         //Lookup 폼을 불러오기 위해서
+using K_Means.Processing;   //전처리, AI기법을 불러오기 위해서 
+
+namespace K_Means
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        String imagePath;
+
+        public Form1()
+        {
+            //Part #1 초기화 부분 
+            InitializeComponent();
+            imagePath = "C:\\Users\\KYJ\\Desktop\\Git Hub\\K-Means\\K-Means\\Image\\Observer.jpg";
+            bitmap = new Bitmap(imagePath);
+
+            //Part #2 
+            Pretreatment prtment = new Pretreatment(new Bitmap(bitmap));
+            prtment.Gray();
+
+            // Part #3 A.I 
+            AI aI = new AI(new Bitmap(prtment.getBitmap()));
+            aI.Kmeans();
+            
+
+            // Part #4 View
+            Lookup lookup = new Lookup(aI.getBitmap());
+            lookup.Show();
+
+            pictureBox1.Image = bitmap;
+            pictureBox2.Image = prtment.getBitmap();
+            pictureBox3.Image = aI.getBitmap();
+        }
+    }
+}
---
+++ Processing/Pretreatment.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_Means.Processing
+{
+    class Pretreatment
+    {
+        Bitmap bitmap;
+        int[,,] color; //R = 0,x,y 
+                       //G = 1,x,y 
+                       //B = 2,x,y
+
+        public Pretreatment(Bitmap orignalBitamp)
+        {
+            this.bitmap = orignalBitamp;
+            color = new int[3, orignalBitamp.Width, orignalBitamp.Height];
+
+            for (int x = 0; x < orignalBitamp.Width; x++)
+                for (int y = 0; y < orignalBitamp.Height; y++)
+                {
+                    color[0, x, y] = orignalBitamp.GetPixel(x, y).R;
+                    color[1, x, y] = orignalBitamp.GetPixel(x, y).G;
+                    color[2, x, y] = orignalBitamp.GetPixel(x, y).B;
+                }
+        }
+
+        #region Gray
+        /* 
+        Gray 
+        3차원의 RGB값을 한계의 값으로 바꿔서 처리의 효율성을 높이기 위한 작업으로
+        R = G = B 값이 전부 같다
+        
+        방법은 3가지가 있으나
+        G,B 값을 -> R값으로 처리해주는 기법을 사용한다.
+        */
+        public void Gray()
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    color[0, x, y] = bitmap.GetPixel(x, y).R;
+                    color[1, x, y] = bitmap.GetPixel(x, y).R;
+                    color[2, x, y] = bitmap.GetPixel(x, y).R;
+                }
+        }
+
+        #endregion
+       
+
+        public Bitmap getBitmap()
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    bitmap.SetPixel(x, y, Color.FromArgb(color[0, x, y], color[1, x, y], color[2, x, y]));
+                }
+            return bitmap;
+        }
+
+    }
+}
